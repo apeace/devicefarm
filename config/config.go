8f7975463c06bc0f78970c059941286e6658cfef
Andrew Peace <apeace@gmail.com>
2016-04-18 16:26:31 -0400

    Simplify config module and use testify assertions
    Rename BuildConfig to BuildManifest

--- config/config.go
+++ config/config.go
@@ -81,39 +81,9 @@ type Device string
 // A DeviceGroup is just a list of Devices.
 type DeviceGroup []Device
 
-func (dg *DeviceGroup) Equals(dg2 DeviceGroup) bool {
-	dg1 := *dg
-	if len(dg1) != len(dg2) {
-		return false
-	}
-	existsInDg1 := map[Device]bool{}
-	for _, device := range dg1 {
-		existsInDg1[device] = true
-	}
-	for _, device := range dg2 {
-		if !existsInDg1[device] {
-			return false
-		}
-	}
-	return true
-}
-
 // A BuildSteps is just a list of strings: the bash commands to execute in order
 // to perform the build.
 type BuildSteps []string
-
-func (bs *BuildSteps) Equals(bs2 BuildSteps) bool {
-	bs1 := *bs
-	if len(bs1) != len(bs2) {
-		return false
-	}
-	for i := range bs1 {
-		if bs2[i] != bs1[i] {
-			return false
-		}
-	}
-	return true
-}
 
 // An AndroidConfig specifies the location of APKs after running the build steps.
 type AndroidConfig struct {
@@ -121,41 +91,21 @@ type AndroidConfig struct {
 	ApkInstrumentation string `yaml:"apk_instrumentation"`
 }
 
-func (c1 *AndroidConfig) Equals(c2 *AndroidConfig) bool {
-	return c1.Apk == c2.Apk && c1.ApkInstrumentation == c2.ApkInstrumentation
-}
-
-// A BuildConfig specifies the whole configuration for a build: the steps to
+// A BuildManifest specifies the whole configuration for a build: the steps to
 // perform the build, the location of Android APKs, and the DeviceGroup names
 // to run on.
-type BuildConfig struct {
+type BuildManifest struct {
 	Steps            BuildSteps    `yaml:"build"`
 	Android          AndroidConfig `yaml:"android"`
 	DeviceGroupNames []string      `yaml:"devicegroups"`
 }
 
-func (c1 *BuildConfig) Equals(c2 *BuildConfig) bool {
-	if len(c1.DeviceGroupNames) != len(c2.DeviceGroupNames) {
-		return false
-	}
-	existsInDg1 := map[string]bool{}
-	for _, deviceGroup := range c1.DeviceGroupNames {
-		existsInDg1[deviceGroup] = true
-	}
-	for _, deviceGroup := range c2.DeviceGroupNames {
-		if !existsInDg1[deviceGroup] {
-			return false
-		}
-	}
-	return c1.Steps.Equals(c2.Steps) && c1.Android.Equals(&c2.Android)
-}
-
 // A Config specifies configuration for a particular repo: the names of DeviceGroups,
-// the default BuildConfig, and override BuildConfigs for particular branches.
+// the default BuildManifest, and override BuildManifests for particular branches.
 type Config struct {
-	DeviceGroupDefinitions map[string]DeviceGroup `yaml:"devicegroups"`
-	Defaults               BuildConfig            `yaml:"defaults"`
-	Branches               map[string]BuildConfig `yaml:"branches"`
+	DeviceGroupDefinitions map[string]DeviceGroup   `yaml:"devicegroups"`
+	Defaults               BuildManifest            `yaml:"defaults"`
+	Branches               map[string]BuildManifest `yaml:"branches"`
 }
 
 // Creates a new Config from a YAML file.
@@ -186,27 +136,4 @@ func (config *Config) IsValid() (bool, error) {
 		}
 	}
 	return true, nil
-}
-
-func (c1 *Config) Equals(c2 *Config) bool {
-	if len(c1.DeviceGroupDefinitions) != len(c2.DeviceGroupDefinitions) {
-		return false
-	}
-	for k, v1 := range c1.DeviceGroupDefinitions {
-		if v2, ok := c2.DeviceGroupDefinitions[k]; !ok || !v1.Equals(v2) {
-			return false
-		}
-	}
-	if !c1.Defaults.Equals(&c2.Defaults) {
-		return false
-	}
-	if len(c1.Branches) != len(c2.Branches) {
-		return false
-	}
-	for k, v1 := range c1.Branches {
-		if v2, ok := c2.Branches[k]; !ok || !v1.Equals(&v2) {
-			return false
-		}
-	}
-	return true
 }
--- config/config_test.go
+++ config/config_test.go
@@ -1,115 +1,43 @@
 package config
 
-import "testing"
-
-func TestDeviceGroupEquals(t *testing.T) {
-	d1 := DeviceGroup{"foo", "bar"}
-	d2 := DeviceGroup{"bar", "foo"}
-	d3 := DeviceGroup{"foo"}
-	d4 := DeviceGroup{"foo", "baz"}
-	if !d1.Equals(d2) {
-		t.Errorf("Expected %v to equal %v", d1, d2)
-	}
-	if d1.Equals(d3) {
-		t.Errorf("Expected %v to NOT equal %v", d1, d3)
-	}
-	if d1.Equals(d4) {
-		t.Errorf("Expected %v to NOT equal %v", d1, d4)
-	}
-}
-
-func TestBuildStepEquals(t *testing.T) {
-	b1 := BuildSteps{"foo", "bar"}
-	b2 := BuildSteps{"foo", "bar"}
-	b3 := BuildSteps{"bar", "foo"}
-	b4 := BuildSteps{"foo"}
-	b5 := BuildSteps{"foo", "baz"}
-	if !b1.Equals(b2) {
-		t.Errorf("Expected %v to equal %v", b1, b2)
-	}
-	if b1.Equals(b3) {
-		t.Errorf("Expected %v to NOT equal %v", b1, b3)
-	}
-	if b1.Equals(b4) {
-		t.Errorf("Expected %v to NOT equal %v", b1, b5)
-	}
-	if b1.Equals(b4) {
-		t.Errorf("Expected %v to NOT equal %v", b1, b5)
-	}
-}
-
-func TestBuildConfigEquals(t *testing.T) {
-	b1 := BuildConfig{DeviceGroupNames: []string{"foo", "bar"}}
-	b2 := BuildConfig{DeviceGroupNames: []string{"bar", "foo"}}
-	b3 := BuildConfig{DeviceGroupNames: []string{"foo"}}
-	b4 := BuildConfig{DeviceGroupNames: []string{"foo", "baz"}}
-	if !b1.Equals(&b2) {
-		t.Errorf("Expected %v to equal %v", b1, b2)
-	}
-	if b1.Equals(&b3) {
-		t.Errorf("Expected %v to NOT equal %v", b1, b3)
-	}
-	if b1.Equals(&b4) {
-		t.Errorf("Expected %v to NOT equal %v", b1, b4)
-	}
-}
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestConfigIsValid(t *testing.T) {
-	c1 := Config{DeviceGroupDefinitions: map[string]DeviceGroup{"foo": []Device{"bar"}}}
-	c2 := Config{DeviceGroupDefinitions: map[string]DeviceGroup{"foo": []Device{}}}
-	c3 := Config{}
-	if ok, err := c1.IsValid(); !ok {
-		t.Errorf("Expected %v to be valid but got %v", c1, err)
-	}
-	if ok, _ := c2.IsValid(); ok {
-		t.Errorf("Expected %v to be invalid", c2)
-	}
-	if ok, _ := c3.IsValid(); ok {
-		t.Errorf("Expected %v to be invalid", c3)
-	}
-}
+	assert := assert.New(t)
 
-func TestConfigEquals(t *testing.T) {
 	c1 := Config{DeviceGroupDefinitions: map[string]DeviceGroup{"foo": []Device{"bar"}}}
-	c2 := Config{DeviceGroupDefinitions: map[string]DeviceGroup{"foo": []Device{"bar"}}}
-	c3 := Config{DeviceGroupDefinitions: map[string]DeviceGroup{"foo": []Device{"baz"}}}
-	c4 := Config{DeviceGroupDefinitions: map[string]DeviceGroup{"foo": []Device{"baz"}, "bar": []Device{"blah"}}}
-	if !c1.Equals(&c2) {
-		t.Errorf("Expected %v to equal %v", c1, c2)
-	}
-	if c1.Equals(&c3) {
-		t.Errorf("Expected %v to NOT equal %v", c1, c3)
-	}
-	if c1.Equals(&c4) {
-		t.Errorf("Expected %v to NOT equal %v", c1, c4)
-	}
-	c5 := Config{Defaults: BuildConfig{Steps: BuildSteps{"foo", "bar"}}}
-	c6 := Config{Defaults: BuildConfig{Steps: BuildSteps{"bar", "foo"}}}
-	if c5.Equals(&c6) {
-		t.Errorf("Expected %v to NOT equal %v", c5, c6)
-	}
-	c7 := Config{Branches: map[string]BuildConfig{"master": BuildConfig{}}}
-	c8 := Config{Branches: map[string]BuildConfig{"staging": BuildConfig{}}}
-	c9 := Config{Branches: map[string]BuildConfig{"staging": BuildConfig{}, "master": BuildConfig{}}}
-	if c7.Equals(&c8) {
-		t.Errorf("Expected %v to NOT equal %v", c7, c8)
-	}
-	if c7.Equals(&c9) {
-		t.Errorf("Expected %v to NOT equal %v", c7, c9)
-	}
+	ok, err := c1.IsValid()
+	assert.True(ok)
+	assert.Nil(err)
+
+	// invalid due to empty device group def
+	c2 := Config{DeviceGroupDefinitions: map[string]DeviceGroup{"foo": []Device{}}}
+	ok, err = c2.IsValid()
+	assert.False(ok)
+	assert.NotNil(err)
+
+	// invalid due to no device group defs
+	c3 := Config{}
+	ok, err = c3.IsValid()
+	assert.False(ok)
+	assert.NotNil(err)
 }
 
 func TestNew(t *testing.T) {
+	assert := assert.New(t)
+
 	config, err := New("testdata/config.yml")
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(err)
+
 	deviceGroupDefs := map[string]DeviceGroup{
 		"a_few_devices": {"Samsung S3", "Blah fone"},
 		"samsung_s4_s5": {"Samsung S4 TMobile", "Samsung S4 AT&T", "Samsung S5 TMobile", "Samsung S5 AT&T"},
 		"everything":    {"+a_few_devices", "+samsung_s4_s5"},
 	}
-	defaultBuild := BuildConfig{
+	defaultBuild := BuildManifest{
 		Steps: BuildSteps{"echo \"Foo\"", "echo \"Bar\""},
 		Android: AndroidConfig{
 			Apk:                "./path/to/build.apk",
@@ -117,46 +45,33 @@ func TestNew(t *testing.T) {
 		},
 		DeviceGroupNames: []string{"a_few_devices"},
 	}
-	masterBuild := BuildConfig{
+	masterBuild := BuildManifest{
 		DeviceGroupNames: []string{"everything"},
 	}
-	expected := &Config{
+	expected := Config{
 		DeviceGroupDefinitions: deviceGroupDefs,
 		Defaults:               defaultBuild,
-		Branches:               map[string]BuildConfig{"master": masterBuild},
+		Branches:               map[string]BuildManifest{"master": masterBuild},
 	}
-	if !expected.Equals(config) {
-		t.Errorf("Expected %v to equal %v", expected, config)
-	}
+	assert.Equal(expected, *config)
 }
 
 func TestNewInvalid(t *testing.T) {
+	assert := assert.New(t)
+
 	config, err := New("testdata/config_invalid.yml")
-	if err == nil {
-		t.Errorf("Expected non-nil error, got %v", err)
-	}
-	if config != nil {
-		t.Errorf("Expected nil result, got %v", config)
-	}
+	assert.NotNil(err)
+	assert.Nil(config)
+
 	config, err = New("testdata/non_existant.yml")
-	if err == nil {
-		t.Errorf("Expected non-nil error, got %v", err)
-	}
-	if config != nil {
-		t.Errorf("Expected nil result, got %v", config)
-	}
+	assert.NotNil(err)
+	assert.Nil(config)
+
 	config, err = New("testdata/config_bad.yml")
-	if err == nil {
-		t.Error("Expected non-nil error")
-	}
-	if config != nil {
-		t.Errorf("Expected nil result, got %v", config)
-	}
+	assert.NotNil(err)
+	assert.Nil(config)
+
 	config, err = New("testdata/config_incomplete.yml")
-	if err == nil {
-		t.Error("Expected non-nil error")
-	}
-	if config != nil {
-		t.Errorf("Expected nil result, got %v", config)
-	}
+	assert.NotNil(err)
+	assert.Nil(config)
 }
