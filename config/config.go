dd149ef427b5f970f4274f7b828d4dcd8e028783
Andrew Peace <apeace@gmail.com>
2016-04-18 16:15:08 -0400

    Build package (#2)
    Simplify config module and use testify assertions
    
    Rename BuildConfig to BuildManifest
    
    More functions for working with configs
    
    Circle config

--- config/config.go
+++ config/config.go
@@ -81,39 +81,9 @@ type Device string
 // A DeviceGroup is just a list of Devices.
 type DeviceGroup []Device
 
-func (dg *DeviceGroup) Equals(dg2 DeviceGroup) bool {
-	dg1 := *dg
-	if len(dg1) != len(dg2) {
-		return false
-	}
-	existsInDg1 := map[Device]bool{}
-	for _, device := range dg1 {
-		existsInDg1[device] = true
-	}
-	for _, device := range dg2 {
-		if !existsInDg1[device] {
-			return false
-		}
-	}
-	return true
-}
-
 // A BuildSteps is just a list of strings: the bash commands to execute in order
 // to perform the build.
 type BuildSteps []string
-
-func (bs *BuildSteps) Equals(bs2 BuildSteps) bool {
-	bs1 := *bs
-	if len(bs1) != len(bs2) {
-		return false
-	}
-	for i := range bs1 {
-		if bs2[i] != bs1[i] {
-			return false
-		}
-	}
-	return true
-}
 
 // An AndroidConfig specifies the location of APKs after running the build steps.
 type AndroidConfig struct {
@@ -121,41 +91,62 @@ type AndroidConfig struct {
 	ApkInstrumentation string `yaml:"apk_instrumentation"`
 }
 
-func (c1 *AndroidConfig) Equals(c2 *AndroidConfig) bool {
-	return c1.Apk == c2.Apk && c1.ApkInstrumentation == c2.ApkInstrumentation
-}
-
-// A BuildConfig specifies the whole configuration for a build: the steps to
+// A BuildManifest specifies the whole configuration for a build: the steps to
 // perform the build, the location of Android APKs, and the DeviceGroup names
 // to run on.
-type BuildConfig struct {
+type BuildManifest struct {
 	Steps            BuildSteps    `yaml:"build"`
 	Android          AndroidConfig `yaml:"android"`
 	DeviceGroupNames []string      `yaml:"devicegroups"`
 }
 
-func (c1 *BuildConfig) Equals(c2 *BuildConfig) bool {
-	if len(c1.DeviceGroupNames) != len(c2.DeviceGroupNames) {
-		return false
+// MergeManfiests merges together two BuildManifests, giving the second manifest
+// priority. In other words, any non-blank field in the second manifest will
+// override the value in the first manifest.
+func MergeManifests(m1 *BuildManifest, m2 *BuildManifest) *BuildManifest {
+	merged := &BuildManifest{}
+	if len(m2.Steps) > 0 {
+		merged.Steps = m2.Steps[:]
+	} else {
+		merged.Steps = m1.Steps[:]
 	}
-	existsInDg1 := map[string]bool{}
-	for _, deviceGroup := range c1.DeviceGroupNames {
-		existsInDg1[deviceGroup] = true
+	if len(m2.Android.Apk) > 0 {
+		merged.Android.Apk = m2.Android.Apk
+	} else {
+		merged.Android.Apk = m1.Android.Apk
 	}
-	for _, deviceGroup := range c2.DeviceGroupNames {
-		if !existsInDg1[deviceGroup] {
-			return false
-		}
+	if len(m2.Android.ApkInstrumentation) > 0 {
+		merged.Android.ApkInstrumentation = m2.Android.ApkInstrumentation
+	} else {
+		merged.Android.ApkInstrumentation = m1.Android.ApkInstrumentation
 	}
-	return c1.Steps.Equals(c2.Steps) && c1.Android.Equals(&c2.Android)
+	if len(m2.DeviceGroupNames) > 0 {
+		merged.DeviceGroupNames = m2.DeviceGroupNames[:]
+	} else {
+		merged.DeviceGroupNames = m1.DeviceGroupNames[:]
+	}
+	return merged
+}
+
+// IsRunnable returns true and nil if the BuildManifest is properly configured
+// to run, and returns false and an error otherwise. For example, if a BuildManifest
+// has no DeviceGroupNames, it cannot be run.
+func (manifest *BuildManifest) IsRunnable() (bool, error) {
+	if len(manifest.Android.Apk) == 0 || len(manifest.Android.ApkInstrumentation) == 0 {
+		return false, fmt.Errorf("Missing Android apk or apk_instrumentation")
+	}
+	if len(manifest.DeviceGroupNames) == 0 {
+		return false, fmt.Errorf("Missing devicegroups")
+	}
+	return true, nil
 }
 
 // A Config specifies configuration for a particular repo: the names of DeviceGroups,
-// the default BuildConfig, and override BuildConfigs for particular branches.
+// the default BuildManifest, and override BuildManifests for particular branches.
 type Config struct {
-	DeviceGroupDefinitions map[string]DeviceGroup `yaml:"devicegroups"`
-	Defaults               BuildConfig            `yaml:"defaults"`
-	Branches               map[string]BuildConfig `yaml:"branches"`
+	DeviceGroupDefinitions map[string]DeviceGroup   `yaml:"devicegroups"`
+	Defaults               BuildManifest            `yaml:"defaults"`
+	Branches               map[string]BuildManifest `yaml:"branches"`
 }
 
 // Creates a new Config from a YAML file.
@@ -176,6 +167,15 @@ func New(filename string) (*Config, error) {
 	return &config, nil
 }
 
+// IsValid returns true and nil if the Config is valid, and returns false and
+// an error otherwise. Valid does not necessesarily mean that all builds will
+// work, it just means that the config does not have any obvious errors.
+//
+// For example, a config with no `defaults` and no config for the `master`
+// branch could be valid. But if you try to run a build on the `master` branch
+// the build will still fail, because there is no config available.
+//
+// See also BuildManifest#IsRunnable().
 func (config *Config) IsValid() (bool, error) {
 	if len(config.DeviceGroupDefinitions) == 0 {
 		return false, fmt.Errorf("devicegroups must have at least one group")
@@ -188,25 +188,13 @@ func (config *Config) IsValid() (bool, error) {
 	return true, nil
 }
 
-func (c1 *Config) Equals(c2 *Config) bool {
-	if len(c1.DeviceGroupDefinitions) != len(c2.DeviceGroupDefinitions) {
-		return false
+// BranchManifest returns a BuildManifest for the given branch name, by starting
+// from the Defaults manifest (if any) and merging branch-specific overrides on
+// top of it.
+func (config *Config) BranchManifest(branch string) *BuildManifest {
+	manifest := MergeManifests(&BuildManifest{}, &config.Defaults)
+	if branchOverrides, ok := config.Branches[branch]; ok {
+		manifest = MergeManifests(manifest, &branchOverrides)
 	}
-	for k, v1 := range c1.DeviceGroupDefinitions {
-		if v2, ok := c2.DeviceGroupDefinitions[k]; !ok || !v1.Equals(v2) {
-			return false
-		}
-	}
-	if !c1.Defaults.Equals(&c2.Defaults) {
-		return false
-	}
-	if len(c1.Branches) != len(c2.Branches) {
-		return false
-	}
-	for k, v1 := range c1.Branches {
-		if v2, ok := c2.Branches[k]; !ok || !v1.Equals(&v2) {
-			return false
-		}
-	}
-	return true
+	return manifest
 }
